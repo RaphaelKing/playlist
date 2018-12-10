50a3c37c3eb290c629dc58208246da139808a24d
Emily Garvey <emilyg@scripted.org>
2018-12-10 20:07:04 +0000

    forked

--- js/script.js
+++ js/script.js
@@ -1,47 +1,61 @@
 /* global $ */
 
 // BELOW Update the songs to your favorites songs. There should be at least 4.
-var songs = [
-    "24 Karat Magic",
-    "Nice For What",
-    "Universe"
+var shows = [
+    "Bob's Burgers",
+    "Parks and Recreation",
+    "This is Us",
+    "The Handmaid's Tale"
 ];
 // BELOW Add 4 More arrays to store images_links, atists, song lengths, and links for each song
 // Make sure they match the same order as your array above
+var networks = [
+    
+];
+
+var episodes = [
+    
+];
+
+var image_links = [
+    
+];
+
+var IMDB_links = [
+    
+];
 
 
 
 
-
-
-function displaySongInfo(){
-    songs.forEach(function(song) {
-        $("#songs").append("<p>" + song + "</p>"); 
+function displayShowInfo(){
+    shows.forEach(function(name) {
+        $("#shows").append("<p>" + name + "</p>"); 
     });
     // BELOW Use forEach Loop to display the data from each of your array's in the correct div
-
+    displayShowInfo(shows);
 }
 
-function emptySongInfo(){
-    $("#songs").empty();
+function emptyShowInfo(){
+    $("#shows").empty();
     // Use jQuery to empty all of the remaining divs
 
 
 }
 
 
-function addSongInfo(){
-    var songName = $("#song").val();
-    songs.push(songName);
+function addShowInfo(){
+    var showName = $("#show").val();
+    shows.push(showName);
     // BELOW write the code to add new items to each of the other arrays
 
 
 }
 
 $("#add").click(function() {
-    emptySongInfo();
-    addSongInfo();
-    displaySongInfo();
+    emptyShowInfo();
+    addShowInfo();
+    displayShowInfo();
 });
 
-displaySongInfo();
+displayShowInfo();
